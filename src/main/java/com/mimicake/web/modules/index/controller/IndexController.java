c9bb0cc480982e89ef837ded85c58465cbeded43
Feco <260546078@qq.com>
2017-06-22 15:23:54 +0800

    mimicake项目提交

---
+++ src/main/java/com/mimicake/web/modules/index/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.mimicake.web.modules.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 首页 
+ */
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController{
+	
+	@RequestMapping(value = "/index",method = {RequestMethod.GET,RequestMethod.POST})
+	public String index(ModelMap modelMap) {
+
+		return "/index/index";
+	}
+	
+	
+}
